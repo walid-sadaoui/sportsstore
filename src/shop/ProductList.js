d949645a1e0c2ce73ef530aff8a4b9965b5d274d
Walid SADAOUI <walidsadaoui@hotmail.fr>
2020-04-13 19:30:44 +0200

    extending the data store

--- src/shop/ProductList.js
+++ src/shop/ProductList.js
@@ -8,7 +8,10 @@ export class ProductList extends Component {
             return this.props.products.map(p =>
                 <div className="card m-1 p-1 bg-light" key={ p.id }>
                     <h4>
-                        { p.name }<span className="badge badge-pill badge-primary float-right">${ p.price.toFixed(2) }</span > 
+                        { p.name }
+                        <span className="badge badge-pill badge-primary float-right">
+                            ${ p.price.toFixed(2) }
+                        </span > 
                     </h4>
                     <div className="card-text bg-white p-1">
                         { p.description }
