91593f8b89135397568f8a42418ba4f8d96134f3
Walid SADAOUI <walidsadaoui@hotmail.fr>
2020-04-13 17:47:21 +0200

    improving category selection buttons

--- src/shop/CategoryNavigation.js
+++ src/shop/CategoryNavigation.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { ToggleLink } from "../ToggleLink";
 
 export class CategoryNavigation extends Component {
     render() {
             return <React.Fragment> 
-                <Link className = "btn btn-secondary btn-block"
-                to = {this.props.baseUrl} > All </Link>
+                <ToggleLink exact={ true } to = {this.props.baseUrl} > All </ToggleLink>
                 { this.props.categories && this.props.categories.map(cat =>
-                    <Link className="btn btn-secondary btn-block" key={ cat }
-                        to={ `${this.props.baseUrl}/$ { cat.toLowerCase()}`}>{ cat }
-                    </Link>
+                    <ToggleLink key={ cat }
+                        to={ `${this.props.baseUrl}/${ cat.toLowerCase()}`}>
+                        { cat }
+                    </ToggleLink>
                     )
                 }
             </React.Fragment>
